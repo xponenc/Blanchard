9b1d3cbb15762076a8cb28b2a850cecb293340ac
Oleg Boykov <xponenc@gmail.com>
2021-06-17 08:47:49 +0400

    768 pp

--- js/modal.js
+++ js/modal.js
@@ -24,9 +24,9 @@ $(document).ready(function () {
         var index = $('.gallery__slide').index(this);
         // Изменение поведения модального окна от размеров экрана
         var width = screen.width
-        if ((width > 1024 ) || (width <= 768 )) {
+        if ((width > 1024) || (width <= 768)) {
             var bgImage = $(this).css('background-image');
-    
+
             var header = $(this).children('.gallery__slide__author').text()
             var subheader = $(this).children('.gallery__slide__pict-name').text()
             var subSubheader = $(this).children('.gallery__slide-create-years').text()
@@ -35,17 +35,20 @@ $(document).ready(function () {
             $('.modal-guts__subheader').text(subheader)
             $('.modal-guts__sub-subheader').text(subSubheader)
             $('.modal-guts__description').text(info)
-      
-    
+
+
             // let src = 'img/gallery/' + (index + 1) + '.jpg';
             // $('.modal-guts__img').attr('src', src);
             $('.modal-guts__img').css('background-image', bgImage);
-    
+
             $('.modal').toggleClass("closed")
             $('.modal-overlay').toggleClass("closed")
+            $('body').css('overflow', 'hidden')
+            
+
         }
         else {
-            $('.gallery__slider__control').after('<div class="modal-1024 closed"><button class="modal__close-button btn"></button><div class="modal-guts"><div class="modal-guts__img"></div><div class="modal-guts__info"><h1 class="modal-guts__header"></h1><h2 class="modal-guts__subheader"></h2><h3 class="modal-guts__sub-subheader"></h3><p class="modal-guts__description"></p></div></div></div>');
+            $('.gallery__slider__control').after('<div class="modal-1024"><button class="modal__close-button btn"></button><div class="modal-guts"><div class="modal-guts__img"></div><div class="modal-guts__info"><h1 class="modal-guts__header"></h1><h2 class="modal-guts__subheader"></h2><h3 class="modal-guts__sub-subheader"></h3><p class="modal-guts__description"></p></div></div></div>');
             var bgImage = $(this).css('background-image');
             var header = $(this).children('.gallery__slide__author').text()
             var subheader = $(this).children('.gallery__slide__pict-name').text()
@@ -58,20 +61,21 @@ $(document).ready(function () {
             $('.modal-guts__img').css('background-image', bgImage);
 
             $('.gallery__content').css('position', 'relative')
-            $('.modal-1024').toggleClass("closed")
-
+            // $('.modal-1024').toggleClass("closed")
         }
     });
 
     $('.modal__close-button').on('click', function () {
         console.log('click')
         var width = screen.width
-        if ((width > 1024 ) || (width <= 768 )) {
+        if ((width > 1024) || (width <= 768)) {
             $('.modal').toggleClass("closed")
             $('.modal-overlay').toggleClass("closed")
+            
         }
         else {
-            $('.modal-1024').remove()
+            console.log($('.modal-1024'))
+            $('.modal-1024').fadeOut()
         }
     })
 });
--- js/swiper.js
+++ js/swiper.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
             delay: 9000,
         },
     });
-    
+
     const swiper2 = new Swiper('.gallery__slider', {
         slidesPerView: 1,
         // slidesPerColumn: 2,
@@ -27,7 +27,7 @@ $(document).ready(function () {
                 slidesPerColumn: 2,
                 slidesPerGroup: 2,
                 spaceBetween: 34,
-    
+
             },
             // when window width is >= 640px
             1360: {
@@ -47,16 +47,22 @@ $(document).ready(function () {
             prevEl: '.gallery__slider__control .btn_prev',
         },
     });
-    
-    
+
+
     const swiper3 = new Swiper('.publication__slider', {
         // loop: true,
         // loopFillGroupWithBlank: true,
-        slidesPerGroup: 2,
-        slidesPerColumn: 4,
-        slidesPerView: 2,
-        spaceBetween: 30,
+        // slidesPerGroup: 2,
+        // slidesPerColumn: 4,
+        // slidesPerView: 2,
+        // spaceBetween: 30,
         breakpoints: {
+            240: {
+                slidesPerGroup: 2,
+                slidesPerColumn: 4,
+                slidesPerView: 2,
+                spaceBetween: 30,
+            },
             551: {
                 slidesPerView: 2,
                 slidesPerGroup: 2,
@@ -83,14 +89,19 @@ $(document).ready(function () {
             prevEl: '.publication__slider__control .btn_prev',
         },
     });
-    
+
     const swiper4 = new Swiper('.partners__slider', {
         loop: true,
         slidesPerGroup: 1,
         slidesPerView: 1,
         spaceBetween: 21,
-        centeredSlides: true,
+        // centeredSlides: true,
         breakpoints: {
+            // 240: {
+            //     slidesPerView: 2,
+            //     slidesPerGroup: 2,
+            //     spaceBetween: 34,
+            // },
             768: {
                 slidesPerView: 2,
                 slidesPerGroup: 2,
@@ -117,11 +128,14 @@ $(document).ready(function () {
             prevEl: '.partners__slider__control .btn_prev',
         },
     });
-    
+
     var width = screen.width
-    if (width <= 550 ) {
+    if (width <= 550) {
         console.log('Стартует свайпер events')
         $('.events__list').removeClass('events__list')
+        $('.events__item').addClass('swiper-slide')
+        $('.events__slider').addClass('swiper-container')
+        $('.events__slider__wrapper').addClass('swiper-wrapper')
         const swiper5 = new Swiper('.events__slider', {
             loop: true,
             slidesPerGroup: 1,
