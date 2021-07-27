1a18c20325192a30c35ae46e516f5f74ca40e018
o.boykov <xponenc@gmail.com>
2021-07-27 17:45:58 +0400

    submenu remastered

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,21 @@
 window.onload = function () {
     const target = document.querySelector('header')
-    target.addEventListener("click", documentActions);
-    function documentActions(e) {
+    target.addEventListener("click", headerActions);
+    function headerActions(e) {
         const targetElement = e.target;
         console.log(targetElement)
-
+        // Субменю
+        if (targetElement.classList.contains('nav__bottom-link')) {
+            // Эта проверка блокирует одновременное открытие нескольких пунктов меню
+            if (document.querySelectorAll('.nav__bottom-item_active').length > 0) {
+                document.querySelectorAll('.nav__bottom-item_active').forEach(n => n.classList.remove('nav__bottom-item_active'))
+            }
+            targetElement.closest('.nav__bottom-item').classList.toggle('nav__bottom-item_active');
+        }
+        if (!targetElement.closest('.nav__bottom-item') && document.querySelectorAll('.nav__bottom-item_active').length > 0) {
+            document.querySelectorAll('.nav__bottom-item_active').forEach(n => n.classList.remove('nav__bottom-item_active'))
+        }
+        // Открытие-закрытие формы поиска
         if (window.innerWidth <= 1024) {
             if (!targetElement.closest('.header__search') && document.querySelector('.nav__top-search-toggle').classList.contains('nav__top-search-toggle_hide')) {
                 document.querySelector('.header__search-wrapper').classList.remove('header__search-wrapper_active');
@@ -17,43 +28,23 @@ window.onload = function () {
                 document.querySelector('.header__search-wrapper').classList.add('header__search-wrapper_active');
             }
         }
+        // Открытие-закрытие бургера
+        if (window.innerWidth <= 1360) {
+            if (targetElement.classList.contains('.nav__toggle') || targetElement.closest('.nav__toggle')) {
+                targetElement.classList.toggle('nav__toggle_active')
+                document.querySelector('.nav__top-block').classList.toggle('nav__top-block_active')
+                document.querySelector('.nav__top-sign-in-btn').classList.toggle('btn_simple')
+                document.querySelector('.nav__top-sign-in-btn').classList.toggle('btn')
+            }
+        }
     }
 }
 
-
-$(document).ready(function () {
-    if (window.innerWidth <= 1360) {
-        $('.nav__toggle').on('click', function () {
-            $(this).toggleClass('nav__toggle_active')
-            $('.nav__top-block').toggleClass('nav__top-block_active')
-            $('.nav__top-sign-in-btn').toggleClass('btn_simple btn')
-            $('.nav__top-burger-close').toggleClass('nav__top-burger-close_active')
-        })
-        $('.nav__top-burger-close').on('click', function () {
-            $('.nav__top-block').toggleClass('nav__top-block_active')
-            $('.nav__top-sign-in-btn').toggleClass('btn_simple btn')
-            $('.nav__top-burger-close').toggleClass('nav__top-burger-close_active')
-        })
-    }
-
-    $('.nav__bottom-link').on('click', function () {
-        const btn = this
-        $('.nav__bottom-link').each(function (index, element) {
-            if (element === btn) {
-                $(element).toggleClass('angle-up');
-                $(element).toggleClass('angle-down');
-                $(element).next().toggleClass('sub-menu_active')
-            } else {
-                $(element).addClass('angle-down')
-                $(element).removeClass('angle-up')
-                $(element).next().removeClass('sub-menu_active')
-            }
-        });
-    });
-})
-$('.nav__bottom-sub-menu').each(function (index, element) {
-    new SimpleBar(element, {
+//Инициализация прокруток в пунктах субменю
+Array.prototype.forEach.call(
+    document.querySelectorAll('.nav__bottom-sub-menu'),
+    el => new SimpleBar(el, {
         autoHide: false,
         scrollbarMaxSize: 28,
-    });
-});
+    })
+);
